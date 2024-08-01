6af809840af94ca5d78426535dff14d1455fd701
nareshdt99 <101355763+nareshdt99@users.noreply.github.com>
2024-08-01 12:03:00 +0530

    Update gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "auto-expire" {
-  name          = "no-public-access-bucket"
+  name          = "no-public-access-bucket-nar"
   location      = "US"
   force_destroy = true
   project_id = "hazel-freehold-430616-h4"
