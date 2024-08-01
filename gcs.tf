c527717d815271c55b1fd843bca88651b2eb52ca
nareshdt99 <101355763+nareshdt99@users.noreply.github.com>
2024-08-01 12:01:32 +0530

    Delete gcs.tf

--- gcs.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "no-public-access-bucket"
-  location      = "US"
-  force_destroy = true
-  project_id = "hazel-freehold-430616-h4"
-  public_access_prevention = "enforced"
-}
